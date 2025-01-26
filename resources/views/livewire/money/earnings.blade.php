dd3f875749b21cf180142d2e031b6498fd814927
abdelrahmannshoieeb <abdoshoieb93@gmail.com>
2025-01-26 21:02:27 +0200

    A

--- resources/views/livewire/money/earnings.blade.php
+++ resources/views/livewire/money/earnings.blade.php
@@ -18,9 +18,9 @@
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase   text-center" style="font-size: larger; font-weight: bolder">المبالغ المستحقة من العملاء </th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase   text-center" style="font-size: larger; font-weight: bolder">المديونية للموردين   </th>
                             @if ($earnings_from_customers + $receivable_to_suppliers >= 0) 
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase   text-center" style="font-size: larger; font-weight: bolder">الارباح   </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase   text-center" style="font-size: larger; font-weight: bolder">مديونية   </th>
                             @else
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase   text-center" style="font-size: larger; font-weight: bolder">مديونية </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase   text-center" style="font-size: larger; font-weight: bolder">الارباح </th>
                             @endif
                         </tr>
                     </thead>
